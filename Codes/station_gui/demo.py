ff195c77ff2fea0b58c747486a8815ce6e561869
realtonypark <34884758+realtonypark@users.noreply.github.com>
2022-08-30 20:36:57 +0900

    Added demo gui program

---
+++ Codes/station_gui/demo.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry("750x450")
+root.resizable(False, False)
+root.title("BUZ Station")
+
+#def
+
+
+#Create
+labTitle = ttk.Label(root, text = "BUZ Station", font=("impact", 60))
+btnLaunch = ttk.Button(root, text = "Launch BUZ", padding = 15)
+
+
+#Show
+labTitle.pack()
+btnLaunch.pack()
+
+
+#loop
+root.mainloop()
